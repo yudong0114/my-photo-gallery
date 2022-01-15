d7d9384c791a84d2daf89a8af18d2ec16556671f
yudong0114 <yukisaito114@gmail.com>
2022-01-16 03:17:55 +0900

    [update]noidex対応

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,6 +15,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      { hid: 'robots', name: 'robots', content: 'noindex' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
